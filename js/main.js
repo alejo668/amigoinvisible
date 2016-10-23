00c4dcca1946a5d506fb270eafb3bcdbe888597a
alejo668 <who.knows@com>
2016-10-23 17:40:06 +0100

    test retrieve json

--- js/main.js
+++ js/main.js
@@ -1,21 +1,11 @@
 
 // TODO: ordenar lista de nombres alfabeticamente
 
-var resultData = {
-  "alejo": {pass:"YGGJO", amigo:"mcarmen"},
-  "mcarmen": {pass:"YGGJO", amigo:"mcarmen"},
-  "antonio": {pass:"YGGJO", amigo:"mcarmen"},
-  "cristian": {pass:"YGGJO", amigo:"mcarmen"},
-  "david": {pass:"YGGJO", amigo:"mcarmen"},
-  "encarnita": {pass:"YGGJO", amigo:"mcarmen"},
-  "jose": {pass:"YGGJO", amigo:"mcarmen"},
-  "mjose": {pass:"YGGJO", amigo:"mcarmen"},
-  "alberto": {pass:"YGGJO", amigo:"mcarmen"},
-  "sonia": {pass:"YGGJO", amigo:"mcarmen"},
-  "jesus": {pass:"YGGJO", amigo:"mcarmen"},
-  "loli": {pass:"YGGJO", amigo:"mcarmen"},
-  "mari": {pass:"YGGJO", amigo:"mcarmen"}
-};
+var resultData = $.getJSON("data.json", function(data){
+    $.each(data.content, function(i, field){
+        console.log("field: " + field);
+    });
+});
 
 var form = document.getElementById("search-result");
 // form.noValidate = true;
@@ -25,8 +15,9 @@ var button = document.getElementById("send");
 button.addEventListener("click", function () {
   showResult();
 });
-
-// TODO: button.addEventListener("touchend", findParticipant);
+button.addEventListener("touchend", function () {
+  showResult();
+});
 
 var showResult = function() {
   var user = document.getElementById("user").value;
